98064ecc0d3384e519bc7ed63b79b370d258b554
link00000000 <crandall.logan@gmail.com>
2017-03-12 16:18:45 -0400

    Fixed bower gulp task not clearing target directory and bower-watch task now checks for new files on init.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-/*jslint node:true */
-"use strict";
+/*jslint node: true, sloppy: true*/
 
 var gulp = require('gulp');
 var server = require('gulp-express');
 var rimraf = require('rimraf');
 var bower = require('gulp-bower');
 var watch = require('gulp-watch');
+var exec = require('child_process').exec;
 
 gulp.task('default', function () {
     server.run(['index.js']);
@@ -16,11 +16,14 @@ gulp.task('default', function () {
 });
 
 gulp.task('bower', function () {
-    return bower('./bower_components')
-        .pipe(gulp.dest('public/dist/'));
+    return rimraf('public/dist', function () {
+        bower('./bower_components')
+            .pipe(gulp.dest('public/dist/'));
+    });
 });
 
 gulp.task('bower-watch', function () {
+    exec('gulp bower');
     watch('bower.json', function () {
         rimraf('public/dist', function () {
             bower('./bower_components')
